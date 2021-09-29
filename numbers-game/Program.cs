af4f5b58ea3f16ef7fe08afe9c5561edae95c096
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-29 16:38:43 -0500

    prompts user for arr size and populates arr with user input data

--- numbers-game/Program.cs
+++ numbers-game/Program.cs
@@ -6,7 +6,29 @@ namespace numbers_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartSequence();
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Enter a number greater than zero.");
+            string userData = Console.ReadLine();
+            int arrSize = Convert.ToInt32(userData);
+            int[] numArray = new int[arrSize];
+            Populate(numArray);
+        }
+
+        static void Populate(int[] numArr)
+        { 
+            int startingIndex = 1;
+            for(int i = 0; i < numArr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number {startingIndex}/{numArr.Length}");
+                string dataToAdd = Console.ReadLine();
+                int numToAdd = Convert.ToInt32(dataToAdd);
+                numArr[i] = numToAdd;
+                startingIndex++;
+            }
         }
     }
 }
