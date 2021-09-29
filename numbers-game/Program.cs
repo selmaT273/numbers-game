e0d7a7e8fa2b9c15f3b9116404d8ccb9ccba3bb0
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-29 17:03:44 -0500

    calculating sum and printing array size and contents

--- numbers-game/Program.cs
+++ numbers-game/Program.cs
@@ -14,11 +14,16 @@ namespace numbers_game
             Console.WriteLine("Enter a number greater than zero.");
             string userData = Console.ReadLine();
             int arrSize = Convert.ToInt32(userData);
-            int[] numArray = new int[arrSize];
-            Populate(numArray);
+            int[] arrToFill = new int[arrSize];
+            int[] populatedArr = Populate(arrToFill);
+            int finalSum = GetSum(populatedArr);
+            string arrString = String.Join(",", populatedArr);
+            Console.WriteLine($"Your array size is: {arrSize}");
+            Console.WriteLine($"The numbers in the array are {arrString}");
+            Console.WriteLine($"The sum is {finalSum}");
         }
 
-        static void Populate(int[] numArr)
+        static int[] Populate(int[] numArr)
         { 
             int startingIndex = 1;
             for(int i = 0; i < numArr.Length; i++)
@@ -29,6 +34,17 @@ namespace numbers_game
                 numArr[i] = numToAdd;
                 startingIndex++;
             }
+            return numArr;
+        }
+
+        static int GetSum(int[] numArr)
+        {
+            int totalSum = 0;
+            for(int i = 0; i < numArr.Length; i++)
+            {
+                totalSum = totalSum + numArr[i];
+            }
+            return totalSum;
         }
     }
 }
